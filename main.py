acbacca4d54dba031d1bcd3c915f6eacdf54333d
Gaius-Capito <gaius.capito@gmail.com>
2022-02-06 20:10:47 +0300

    add !

--- main.py
+++ main.py
@@ -22,17 +22,17 @@ class Window(tk.Tk):
         frame.place(relx=0, rely=0, relwidth=1, relheight=0.05)
         l_btn = tk.Label(master=frame, text='Кнопочки', bg='#A9A9A9', font=self.FONT)
         l_btn.place(relx=0.08, rely=0.2)
-        l_editor = tk.Label(master=frame, text='Редактор-Транслитатор-Экстерминатор', bg='#A9A9A9', font=self.FONT)
+        l_editor = tk.Label(master=frame, text='Редактор-Транслитатор-Экстерминатор!', bg='#A9A9A9', font=self.FONT)
         l_editor.place(relx=0.6, rely=0.2)
 
     def btns_frame(self):
         frame = tk.Frame(self, height=670, width=300, bg='#C0C0C0')
         frame.place(relx=0, rely=0.05, relwidth=0.2, relheight=1)
-        btn1 = tk.Button(master=frame, text='Кнопочка1',
+        btn1 = tk.Button(master=frame, text='Кнопочка One',
                             bg='#FF69B4', fg='#00FF00', activebackground='#00BFFF', font=self.FONT,
                             activeforeground='#FF0000')
         btn1.place(relx=0.3, rely=0.6)
-        btn2 = tk.Button(master=frame, text='Кнопочка2',
+        btn2 = tk.Button(master=frame, text='Кнопочка Two',
                            bg='#00FF00', fg='#C71585', activebackground='#FFFF00', font=self.FONT,
                            activeforeground='#FF0000')
         btn2.place(relx=0.5, rely=0.4)
